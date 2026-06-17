cc5564b357da3d467433584a9bfe82f862ee8b8e
Luis <luis.h.rios@unl.edu.ec>
2026-06-17 10:23:39 -0500

    Homework: Tarea del dia 17-06-2026

---
+++ Retos/clima.js
@@ -0,0 +1,24 @@
+const seleccionar = document.getElementById("clima");
+const parrafo = document.querySelector("p");
+
+seleccionar.addEventListener("change", establecerClima);
+
+function establecerClima() {
+    let eleccion = seleccionar.value;
+
+    if (eleccion === "soleado") {
+        parrafo.textContent =
+            "El día está agradable y soleado hoy. Ve a la playa o al parque y come un helado.";
+    } else if (eleccion === "lluvioso") {
+        parrafo.textContent =
+            "Está lloviendo, tome un abrigo para lluvia y un paraguas.";
+    } else if (eleccion === "nevando") {
+        parrafo.textContent =
+            "Está nevando ─ ¡está congelando! Lo mejor es quedarse en casa con una taza caliente de chocolate, o hacer un muñeco de nieve.";
+    } else if (eleccion === "nublado") {
+        parrafo.textContent =
+            "No está lloviendo, pero el cielo está gris y nublado; podría llover en cualquier momento, así que lleve un saco por si acaso.";
+    } else {
+        parrafo.textContent = "";
+    }
+}
---
+++ U2/JavaScript/05-clase-condicional.js
@@ -0,0 +1,46 @@
+// Condicional Simple
+let edad = 20;
+
+if (edad >= 18) {
+    console.log("Eres mayor de edad.");
+}
+
+//Condicional doble
+let temperatura = 15;
+
+if (temperatura > 25) {
+    console.log("Hace calor.");
+} else {
+    console.log("Hace frío.");
+}
+
+// Condicional multiple
+let nota = 85;
+
+if (nota >= 90) {
+    console.log("Excelente");
+} else if (nota >= 70) {
+    console.log("Aprobado");
+} else {
+    console.log("Reprobado");
+}
+
+// Switch
+let rol = "admin";
+
+switch (rol) {
+    case "admin":
+        console.log("Tienes acceso completo al sistema.");
+        break;
+
+    case "editor":
+        console.log("Puedes editar contenido.");
+        break;
+
+    case "usuario":
+        console.log("Puedes ver el contenido.");
+        break;
+
+    default:
+        console.log("Rol no reconocido.");
+}
